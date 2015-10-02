b7b43c038217fe01cf5825751a92a273230e3941
edwardekstedt <edwardekstedt@gmail.com>
2015-10-02 11:39:46 +0200

    asd

--- Newton.py
+++ Newton.py
@@ -7,14 +7,25 @@ Created on Fri Oct  2 10:39:19 2015
 
 # Project 2 FMNN25
 
+import numpy as np
+
+
 class optimizationProblem(object):
     
     def __init__(self,function, gradient = None):
         self.f = function
         if not gradient == None:
             self.g = gradient
-
+        else:
+            self.g = 
 class optimizationMethod(object):
     
-    def __init__(self,optimizationProblem):
-        
+    def __init__(self,optimizationProblem,x0 = None):
+        self.op = optimizationProblem
+        if not x0 == None:
+            self.x0 = x0
+        else:
+            x0 = 0 #change to achieve correct dimensions
+            
+
+            
