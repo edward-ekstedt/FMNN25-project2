0ec11e548a797b5530c4ae19707fda6e3c011d7b
edwardekstedt <edwardekstedt@gmail.com>
2015-10-02 10:57:48 +0200

    lite början

--- Newton.py
+++ Newton.py
@@ -6,3 +6,15 @@ Created on Fri Oct  2 10:39:19 2015
 """
 
 # Project 2 FMNN25
+
+class optimizationProblem(object):
+    
+    def __init__(self,function, gradient = None):
+        self.f = function
+        if not gradient == None:
+            self.g = gradient
+
+class optimizationMethod(object):
+    
+    def __init__(self,optimizationProblem):
+        
