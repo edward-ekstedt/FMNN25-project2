417f8519a57a8618df86b3026c182bdd7b0821cb
edwardekstedt <edwardekstedt@gmail.com>
2015-10-02 14:30:50 +0200

    hessian andgradi

--- Newton.py
+++ Newton.py
@@ -8,17 +8,42 @@ Created on Fri Oct  2 10:39:19 2015
 # Project 2 FMNN25
 
 import numpy as np
-
+import scipy as sp
 
 class optimizationProblem(object):
     
-    def __init__(self,function, gradient = None):
+    def __init__(self,function, dimensions, gradient = None):
+        self.dx = 0.0000000001
         self.f = function
-        if not gradient == None:
+        self.dimensions
+        if gradient:
             self.g = gradient
         else:
-            self.g = 
-class optimizationMethod(object):
+            def grad(x):
+                return np.array([(self.f(x+self.dx*delta[i]/2.)-self.f(x-self.dx*delta[i])/2.)/self.dx] for i in range(dimensions))
+            delta = sp.identity(self.dimensions)
+            self.g = grad
+        
+        self.hessian = self.computeHessian()
+        
+    def computeHessian(self):
+        def hess(x):
+            G = np.zeros((self.dimensions,self.dimensions))
+            for row in range(self.dimensions):
+                for col in range(self.dimensions):
+                    G[row,col] = self.secondDerivative(row,col,x)
+            return G
+        return hess
+        
+    def secondDerivative(self,i,j,x):
+        deltax = np.zeros(self.dimensions)
+        deltay = np.zeros(self.dimensions)
+        deltax[i] = self.dx
+        deltay[j] = self.dx
+        return (self.f(x+deltax + deltay)-self.f(x + deltax) -self.f(x + deltay) + self.f(x))/self.dx**2
+        
+
+class Newton(object):
     
     def __init__(self,optimizationProblem,x0 = None):
         self.op = optimizationProblem
@@ -28,4 +53,3 @@ class optimizationMethod(object):
             x0 = 0 #change to achieve correct dimensions
             
 
-            
