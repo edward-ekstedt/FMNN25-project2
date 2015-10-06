e6064080d4be4dc1247f127d801f0951fecc43be
edwardekstedt <edwardekstedt@gmail.com>
2015-10-06 12:47:52 +0200

    finished
    finished version

--- Newton.py
+++ Newton.py
@@ -9,12 +9,12 @@ Created on Fri Oct  2 10:39:19 2015
 
 import numpy as np
 import scipy as sp
-
+import matplotlib.pyplot as plt
 class optimizationProblem(object):
     
     def __init__(self,function, dimensions,tolerance, gradient = None):
         self.tol = tolerance
-        self.dx = 0.00001
+        self.dx = 0.0000001
         self.f = function
         self.dimensions = dimensions
         if gradient:
@@ -49,7 +49,6 @@ class Newton(optimizationProblem):
     
     def step(self,x):
         self.Hinv = np.eye(self.dimensions)
-        k= 1
        # U = np.linalg.cholesky(H)
         #Uinv = np.linalg.inv(U)
         #self.Hinv = np.dot(Uinv,Uinv.T)
@@ -60,6 +59,13 @@ class Newton(optimizationProblem):
             self.deltaX = alpha*sK
             x= x+self.deltaX
             if abs(np.linalg.norm(self.deltaX)) < self.tol:
+                plt.close('all')
+                x1 = np.linspace(-10,10,1000)
+                y = np.linspace(-10,10,1000)
+                X,Y = np.meshgrid(x1,y)
+                F = self.f([X,Y])
+                plt.figure()
+                CS = plt.contour(X,Y,F,20)
                 return x
             self.update(x)
             self.posDefCheck()
@@ -94,17 +100,17 @@ class Newton(optimizationProblem):
         self.sigma = 0.7
         self.tau  =0.1
         self.chi = 9.
-        self.alpha0 = 1.
+        self.alpha0 = sK.dot(sK)/sK.dot(self.Hinv.dot(sK))
         self.alphaL = 0.
         self.alphaU = 10**99
         self.computeValues(xk, sK)
         while not (self.LC() and self.RC()):
             if not self.LC():
-                self.block1()
-                
+                self.block1() 
             else:
                 self.block2()
             self.computeValues(xk, sK)
+            
         return self.alpha0
         
         
@@ -118,31 +124,37 @@ class Newton(optimizationProblem):
         self.f_prime_aL = f_prime_alpha(self.alphaL)
         self.f_prime_a0 = f_prime_alpha(self.alpha0)
     
-#==============================================================================
-#     #Compute the left and right conditions according to Goldstein
-#     def LC(self):
-#         return self.f_alpha0 >= (self.f_alphaL + (1-self.rho)*(self.alpha0 - self.alphaL)*self.f_prime_aL)
-#     def RC(self):
-#         return self.f_alpha0 <= (self.f_alphaL + self.rho*(self.alpha0 - self.alphaL)*self.f_prime_aL)
-# 
-#==============================================================================
-     #Compute the left and right conditions according to Wolfe-Powel
+    #Compute the left and right conditions according to Goldstein
     def LC(self):
-         return self.f_prime_a0 >= self.sigma*self.f_prime_aL
+        return self.f_alpha0 >= (self.f_alphaL + (1-self.rho)*(self.alpha0 - self.alphaL)*self.f_prime_aL)
     def RC(self):
-         return self.f_alpha0 <= self.f_alphaL + self.rho*(self.alpha0 - self.alphaL)*self.f_prime_aL
-
-        
+        return self.f_alpha0 <= (self.f_alphaL + self.rho*(self.alpha0 - self.alphaL)*self.f_prime_aL)
+#==============================================================================
+# 
+#      #Compute the left and right conditions according to Wolfe-Powel
+#     def LC(self):
+#          return self.f_prime_a0 >= self.sigma*self.f_prime_aL
+#     def RC(self):
+#          return self.f_alpha0 <= self.f_alphaL + self.rho*(self.alpha0 - self.alphaL)*self.f_prime_aL
+# 
+#         
+#==============================================================================
     
     def block1(self):
-        deltaAlpha = (self.alpha0-self.alphaL)*(self.f_prime_a0/(self.f_prime_aL-self.f_prime_a0))
+        if not (self.f_prime_aL-self.f_prime_a0):
+            deltaAlpha = 0
+        else:
+            deltaAlpha = (self.alpha0-self.alphaL)*(self.f_prime_a0/(self.f_prime_aL-self.f_prime_a0))
         deltaAlpha = np.max([deltaAlpha, self.tau*(self.alpha0-self.alphaL)])
         deltaAlpha = np.min([deltaAlpha, self.chi*(self.alpha0-self.alphaL)])
         self.alphaL = self.alpha0
         self.alpha0 = self.alpha0 + deltaAlpha
     def block2(self):
         self.alphaU = np.min([self.alpha0,self.alphaU])
-        alphaBar = (self.alpha0-self.alphaL)**2*self.f_prime_aL/(2*(self.f_alphaL-self.f_alpha0 + (self.alpha0-self.alphaL)*self.f_prime_aL))
+        if not (self.f_alphaL-self.f_alpha0 + (self.alpha0-self.alphaL)*self.f_prime_aL):
+            alphaBar = 0
+        else:
+            alphaBar = (self.alpha0-self.alphaL)**2*self.f_prime_aL/(2*(self.f_alphaL-self.f_alpha0 + (self.alpha0-self.alphaL)*self.f_prime_aL))
         alphaBar = np.max([alphaBar,self.alphaL + self.tau*(self.alphaU-self.alphaL)])
         alphaBar = np.min([alphaBar,self.alphaU - self.tau*(self.alphaU-self.alphaL)])
         self.alpha0 = alphaBar
@@ -161,7 +173,9 @@ class badBroyden(Newton):
     def update(self, x):
         gamma = self.g(x)-self.g(x-self.deltaX)
         delta = self.deltaX
-        self.Hinv = self.Hinv + ((gamma - self.Hinv.dot(delta))/(delta.dot(delta))).dot(delta)
+        dtd = delta.dot(delta)
+        Hd = self.Hinv.dot(delta)
+        self.Hinv = self.Hinv + np.dot((gamma - Hd)/dtd,delta)
 class DFP(Newton):
     def update(self,x):
         delta = self.deltaX
@@ -171,9 +185,7 @@ class DFP(Newton):
         Hg = self.Hinv.dot(gamma)
         gtH = gamma.T.dot(self.Hinv)
         self.Hinv = self.Hinv + ddT/dtg - (np.outer(Hg,Hg.T))/(gtH.dot(gamma))
-        
-      
-        
+               
 class BFGS(Newton):
     
     def update(self,x):
@@ -189,8 +201,8 @@ def main():
     def f(x):
         #return x[1]**2 + x[0]**2
         return 100*(x[1]-x[0]**2)**2 + (1 -x[0])**2
-    opt = badBroyden(f,2,0.00000001)
-    x = opt.step([0 ,1.])
+    opt = badBroyden(f,2,10**-9)
+    x = opt.step([1,0])
     print(f(x))
     print(x)
     
